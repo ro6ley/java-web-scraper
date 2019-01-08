1c5510d6e7430a20521ea1fb3d692dc74cb48cb5
Robley <robleyadrian@gmail.com>
2019-01-09 00:46:40 +0300

    initial work

---
+++ src/main/java/com/livescore/scraper/App.java
@@ -0,0 +1,21 @@
+package com.livescore.scraper;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) throws IOException {
+        System.out.println( "Hello World!" );
+
+    }
+
+}
